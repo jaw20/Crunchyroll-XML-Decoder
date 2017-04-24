644269f353f65ee7d73c34e375eac4bcfcad157b
alzamer2 <alzamer2@gmail.com>
2017-04-24 16:21:09 +0300

    change to public folder

--- crunchy-xml-decoder.bat
+++ crunchy-xml-decoder.bat
@@ -1,9 +1,9 @@
 @echo off
-RD "%USERPROFILE%\Documents\Crunchyroll-XML-Decoder_link" 1>NUL 2>NUL
-mklink /j "%USERPROFILE%\Documents\Crunchyroll-XML-Decoder_link" %cd% 1>NUL 2>NUL
-cd "%USERPROFILE%\Documents\Crunchyroll-XML-Decoder_link"
+RD "%PUBLIC%\Crunchyroll-XML-Decoder_link" 1>NUL 2>NUL
+mklink /j "%PUBLIC%\Crunchyroll-XML-Decoder_link" %cd% 1>NUL 2>NUL
+cd "%PUBLIC%\Crunchyroll-XML-Decoder_link"
 :sratre
 crunchy-xml-decoder.py %1 %2 %3 %4 %5 %6 %7 %8 %9
-RD "%USERPROFILE%\Documents\Crunchyroll-XML-Decoder_link" 1>NUL 2>NUL
+RD "%PUBLIC%\Crunchyroll-XML-Decoder_link" 1>NUL 2>NUL
 pause
 
