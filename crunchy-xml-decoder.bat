bc02b844ee42c116d7d2530cac6f37e5f3c2f2ea
alzamer2 <alzamer2@gmail.com>
2017-04-24 16:02:42 +0300

    Fix long path name
    this is fix for long path name in the script as in issue #1

--- crunchy-xml-decoder.bat
+++ crunchy-xml-decoder.bat
@@ -1,5 +1,9 @@
 @echo off
+RD "%USERPROFILE%\Documents\Crunchyroll-XML-Decoder_link" 1>NUL 2>NUL
+mklink /j "%USERPROFILE%\Documents\Crunchyroll-XML-Decoder_link" %cd% 1>NUL 2>NUL
+cd "%USERPROFILE%\Documents\Crunchyroll-XML-Decoder_link"
 :sratre
 crunchy-xml-decoder.py %1 %2 %3 %4 %5 %6 %7 %8 %9
+RD "%USERPROFILE%\Documents\Crunchyroll-XML-Decoder_link" 1>NUL 2>NUL
 pause
 
